49756398d9dd1bed8ccc0015c47174ceae8f286e
bijink <kbijin528.pro@gmail.com>
2023-04-03 19:04:29 +0530

    part 17: custom-font

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import { useCallback } from "react"
+import { View } from "react-native"
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import Home from "./screens/home"
+
+SplashScreen.preventAutoHideAsync()
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Nunito-Regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "Nunito-Bold": require("./assets/fonts/Nunito-Bold.ttf"),
+    "Nunito-Italic": require("./assets/fonts/Nunito-Italic.ttf"),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <View onLayout={onLayoutRootView}>
+      <Home />
+    </View>
+  )
+}
