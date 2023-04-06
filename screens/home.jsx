bad2fdc077b0d8cde01039748bf546d2ba0af527
bijink <kbijin528.pro@gmail.com>
2023-04-06 19:21:20 +0530

    part 22: Passing parameters to routes

--- screens/home.jsx
+++ screens/home.jsx
@@ -1,16 +1,24 @@
-import { View, Text, Button } from "react-native"
+import { View, Text, FlatList, TouchableOpacity } from "react-native"
 import { globalStyles } from "../styles/global"
+import { useState } from "react"
 
 export default function Home({ navigation }) {
-  const pressHandler = () => {
-    navigation.navigate("Review")
-    // navigation.push("Review")
-  }
+  const [reviews, setReviews] = useState([
+    { title: "Zelda, Breath of Fresh Air", rating: 5, body: "lorem ipsum", key: "1" },
+    { title: "Gotta Catch Them All (again)", rating: 4, body: "lorem ipsum", key: "2" },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: "lorem ipsum", key: "3" },
+  ])
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screens</Text>
-      <Button title="see review" onPress={pressHandler} />
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate("Review", item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   )
 }
--- screens/reviewDetails.jsx
+++ screens/reviewDetails.jsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native"
 import { globalStyles } from "../styles/global"
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ route }) {
+  const { title, body, rating } = route.params
+  // console.log(title, body, rating)
+
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
+      <Text>{title}</Text>
+      <Text>{body}</Text>
+      <Text>{rating}</Text>
+      {/* <Text>{JSON.parse(JSON.stringify(title))}</Text> */}
     </View>
   )
 }
